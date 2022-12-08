29fe1203e78b2df1ed1223784a66c8035c238c1b
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-08 18:37:32 +1000

    created functionality of the slider for when you select each option and move slider the sliders stay in their position

--- src/App.js
+++ src/App.js
@@ -76,6 +76,15 @@ function App() {
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const selectedOption = options[selectedOptionIndex]
 
+  function handleSliderChange({ target }) {
+  setOptions(prevOptions => {
+    return prevOptions.map((option, index) => {
+      if (index !== selectedOptionIndex) return option
+      return { ...option, value: target.value }
+    })
+  })
+  }
+
   return (
     <div className='container'>
       <div className='main-image' />
@@ -92,7 +101,10 @@ function App() {
          })}
       </div>
       <Slider 
-      
+      min={selectedOption.range.min}
+      max={selectedOption.range.max}
+      value={selectedOption.value}
+      handleChange={handleSliderChange}
       /> 
     </div>
   )
